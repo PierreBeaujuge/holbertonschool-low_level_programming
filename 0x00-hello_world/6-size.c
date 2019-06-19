cdf01ad2acf51a92c3f3c9db50f5c75a34ab40b1
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-06-19 21:58:33 +0000

    removed extra spaces

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,11 @@
 */
 int main(void)
 {
-  int i = 1, j = 4, k = 4, l = 8, m = 4;
-  printf("Size of a char: %d byte(s)\n", i);
-  printf("Size of an int: %d byte(s)\n", j);
-  printf("Size of a long int: %d byte(s)\n", k);
-  printf("Size of a long long int: %d byte(s)\n", l);
-  printf("Size of a float: %d byte(s)\n", m);
-  return (0);
+int i = 1, j = 4, k = 4, l = 8, m = 4;
+printf("Size of a char: %d byte(s)\n", i);
+printf("Size of an int: %d byte(s)\n", j);
+printf("Size of a long int: %d byte(s)\n", k);
+printf("Size of a long long int: %d byte(s)\n", l);
+printf("Size of a float: %d byte(s)\n", m);
+return (0);
 }
