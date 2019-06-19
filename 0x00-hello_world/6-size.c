5117446b161896fa68bd2aa041f6f4acbb86b766
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-06-19 22:22:45 +0000

    removed hard code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,13 @@
 */
 int main(void)
 {
-int i = 1, j = 4, k = 4, l = 8, m = 4;
-printf("Size of a char: %d byte(s)\n", i);
-printf("Size of an int: %d byte(s)\n", j);
-printf("Size of a long int: %d byte(s)\n", k);
-printf("Size of a long long int: %d byte(s)\n", l);
-printf("Size of a float: %d byte(s)\n", m);
+int integerType, longintegerType, longlongintegerType;
+float floatType;
+char charType;
+printf("Size of a char: %d byte(s)\n", charType);
+printf("Size of an int: %d byte(s)\n", integerType);
+printf("Size of a long int: %d byte(s)\n", longintegerType);
+printf("Size of a long long int: %d byte(s)\n", longlongintegerType);
+printf("Size of a float: %d byte(s)\n", floatType);
 return (0);
 }
