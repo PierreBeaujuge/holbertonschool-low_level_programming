07a2e7747561efe4065cd6a006f33300611aa99e
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-06-20 20:02:40 +0000

    changed slash

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,7 +6,6 @@
 *
 * Return: Always 0 (Success)
 */
-/* betty style doc for function main goes there */
 int main(void)
 {
 int n;
@@ -14,10 +13,10 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-printf("%d is positive/n", n);
+printf("%d is positive\n", n);
 else if (n < 0)
-printf("%d is negative/n", n);
+printf("%d is negative\n", n);
 else
-printf("%d is zero/n", n);
+printf("%d is zero\n", n);
 return (0);
 }
