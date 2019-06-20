f90d7a36deaf0513193bbdba0661fcadc199521a
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-06-20 20:17:45 +0000

    replaced zero

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,6 +18,6 @@ printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 else if (n < 6 && n != 0)
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
 else
-printf("Last digit of %d is %d and is zero\n", n, lastDigit);
+printf("Last digit of %d is %d and is 0\n", n, lastDigit);
 return (0);
 }
