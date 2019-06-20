528c4523141ac2d59acb02eb5a129236c04ca9d4
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-06-20 19:59:04 +0000

    input answers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,11 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 /* betty style doc for function main goes there */
 int main(void)
 {
@@ -9,11 +13,11 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n>0)
-printf ("%d is positive/n", n);
-else if (n=0)
-printf ("%d is zero/n", n);
+if (n > 0)
+printf("%d is positive/n", n);
+else if (n < 0)
+printf("%d is negative/n", n);
 else
-printf ("%d is negative/n", n);
+printf("%d is zero/n", n);
 return (0);
 }
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+char alphabet;
+alphabet = 'a'
+while (alphabet <= 'z')
+putchar(alphabet);
+alphabet++;
+alphabet = 'A'
+while (alphabet <= 'Z')
+putchar(alphabet);
+alphabet++;
+return (0);
+}
