aa4f0fdb56ff4acddada7bc044b26eaa334b021e
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-06-21 06:23:46 +0000

    fixed indentation

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,36 +8,36 @@
 */
 int main(void)
 {
-	int n;
-	int m;
-	int p;
-	int k;
+int n;
+int m;
+int p;
+int k;
 
-	for (n = '0'; n <= '9'; n++)
+for (n = '0'; n <= '9'; n++)
+{
+for (m = '0'; m <= '9'; m++)
+{
+for (p = '0'; p <= '9'; p++)
+{
+for (k = '0'; k <= '9'; k++)
+{
+	if (n <= p && m < k)
 	{
-		for (m = '0'; m <= '9'; m++)
+		putchar(n);
+		putchar(m);
+		putchar(' ');
+		putchar(p);
+		putchar(k);
+		if (!(n == '9' && m == '8' && p == '9' && k == '9'))
 		{
-			for (p = '0'; p <= '9'; p++)
-			{
-				for (k = '0'; k <= '9'; k++)
-				{
-					if (n <= p && m < k)
-					{
-						putchar(n);
-						putchar(m);
-						putchar(' ');
-						putchar(p);
-						putchar(k);
-						if (!(n == '9' && m == '8' && p == '9' && k == '9'))
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-				}
-			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
-	putchar('\n');
-	return (0);
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
