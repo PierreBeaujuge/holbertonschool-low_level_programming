7e09232177d2b1bc14029975672e1871df7b9696
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-06-21 00:19:36 +0000

    input answer

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n;
+	char N;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	for (N = 'A'; N <= 'G'; N++)
+	{
+		putchar(N);
+	}
+	putchar('\n');
+	return (0);
+}
