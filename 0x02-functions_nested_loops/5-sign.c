48b68578a0abba92ea194d594de754f56eb7fc1a
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-06-25 05:20:59 +0000

    input answer

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * print_sign - prints the sign of a function
+ *
+ * int c - is hard-coded in main function
+ *
+ * Return: 0 or 1
+ */
+
+int print_sign(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		i = 1;
+		_putchar('+');
+	}
+	else if (n < 0)
+	{
+		i = -1;
+		_putchar('-');
+	}
+	else
+	{
+		i = 0;
+		_putchar('0');
+	}
+	return (i);
+}
