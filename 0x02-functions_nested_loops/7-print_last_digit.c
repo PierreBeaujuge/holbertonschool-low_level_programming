ec79ca4add2831b18fa1fa5ed5f44f60c051e97c
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-06-26 05:31:45 +0000

    changed some returns

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -16,11 +16,13 @@ int print_last_digit(int n)
 	{
 		i = n % 10;
 		_putchar(i + '0');
+		return (i);
 	}
 	else
 	{
 		n = n * (-1);
 		i = n % 10;
+		_putchar(i + '0');
+		return (i);
 	}
-	return (i);
 }
