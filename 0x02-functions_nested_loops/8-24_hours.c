f087568bb34c21092f93efce00ad14aed0f87be0
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-06-26 04:50:04 +0000

    added if loop

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,7 +15,9 @@ void jack_bauer(void)
 
 	for (i = 0; i <= 2; i++)
 	{
-		for (j = 0; j <= 3; j++)
+		if (i < 2)
+		{
+		for (j = 0; j <= 9; j++)
 		{
 			for (k = 0; k <= 5; k++)
 			{
@@ -27,6 +29,25 @@ void jack_bauer(void)
 					_putchar(k + '0');
 					_putchar(l + '0');
 					_putchar('\n');
+				}
+			}
+		}
+		}
+		if (i == 2)
+		{
+			for (j = 0; j <= 3; j++)
+			{
+				for (k = 0; k <= 5; k++)
+				{
+					for (l = 0; l <= 9; l++)
+					{
+						_putchar(i + '0');
+						_putchar(j + '0');
+						_putchar(':');
+						_putchar(k + '0');
+						_putchar(l + '0');
+						_putchar('\n');
+					}
 				}
 			}
 		}
