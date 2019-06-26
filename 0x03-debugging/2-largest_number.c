cf28eecdeefc3307868cbbbeee4170c5704d9de8
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-06-26 02:56:17 +0000

    all if loops

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,13 +16,13 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (b > a && a > c)
-	{
-		largest = b;
-	}
-	else
+	if (a < b && b < c)
 	{
 		largest = c;
+	}
+	if (b > a && a > c)
+	{
+		largest = b;
 	}
 
 	return (largest);
