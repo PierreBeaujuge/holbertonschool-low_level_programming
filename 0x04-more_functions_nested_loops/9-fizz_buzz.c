a1c5a38c918dc7d8cbc3b4c2a6767d8cffeaaaeb
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-06-27 22:56:19 +0000

    remove extra spacing after 100

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -29,7 +29,14 @@ int main(void)
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			if (i == 100)
+			{
+				printf("Buzz");
+			}
+			else
+			{
+				printf("Buzz ");
+			}
 		}
 	}
 	printf("\n");
