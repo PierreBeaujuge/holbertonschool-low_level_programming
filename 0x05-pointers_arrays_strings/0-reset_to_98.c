8fbad89b562754e60473457c3fbf63f5d49b22bf
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-07-02 21:53:13 +0000

    removed white space

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,7 +4,7 @@
  * reset_to_98 - function that takes a pointer to an int as parameter
  * and updates the value it points to to 98
  *
- * @n: parameter hard-coded in main 
+ * @n: parameter hard-coded in main
  *
  * Return: void
  */
