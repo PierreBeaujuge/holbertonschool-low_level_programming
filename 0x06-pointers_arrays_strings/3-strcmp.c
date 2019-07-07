a7e73d54a00d27c9fa68e5d0dc07d09228a651f9
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-07-07 20:17:52 +0000

    added equal if loop condition

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,18 +12,30 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	char *tmp1 = s1;
-	char *tmp2 = s2;
-
+/*
+ *	char *tmp1 = s1;
+ *	char *tmp2 = s2;
+ */
 	while (*s1 != '\0')
 	{
-		if (*s1 >= *s2)
-			return (*s1 - *s2);
-		else if (*s1 < *s2)
-			return (*s1 - *s2);
-		s1++;
+		if (*s1 == *s2)
+		{
+			s1++;
+			s2++;
+		}
+		else
+		{
+			if (*s1 > *s2)
+				return (*s1 - *s2);
+			else if (*s1 < *s2)
+				return (*s1 - *s2);
+			s1++;
+			s2++;
+		}
 	}
-	s1 = tmp1;
-	s2 = tmp2;
+/*
+ *	s1 = tmp1;
+ *	s2 = tmp2;
+ */
 	return (0);
 }
