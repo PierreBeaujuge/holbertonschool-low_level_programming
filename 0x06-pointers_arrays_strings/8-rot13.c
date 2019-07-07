4e12157f2ec94b1e5347b8522148d697bdcb62dc
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-07-07 22:43:54 +0000

    tried different loop structure

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -11,24 +11,16 @@
 
 char *rot13(char *s)
 {
-	int i;
+	int i, j;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot_it[] =   "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	i = 0;
-	while (s[i] != '\0')
-	{
-		while ((s[i] >= 'a' && s[i] <= 'z') ||
-		       (s[i] >= 'A' && s[i] <= 'Z'))
-		{
-			if ((s[i] >= 'N' && s[i] <= 'Z') ||
-			    (s[i] >= 'n' && s[i] <= 'z'))
-				s[i] -= 13;
-			else
-				s[i] += 13;
-			i++;
-		}
-		i++;
-	}
+	for (i = 0; s[i] != '\0'; i++)
+		for (j = 0; j < 52; j++)
+			if (s[i] == alphabet[j])
+				s[i] = rot_it[j], j = 52;
 	return (s);
+}
 
 /*
  *	char *tmp = s;
@@ -68,4 +60,3 @@ char *rot13(char *s)
  *	s = tmp;
  *return (s);
  */
-}
