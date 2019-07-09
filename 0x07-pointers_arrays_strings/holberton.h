c55ed95b919396a22981663bff02838788fa7c9f
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-07-09 23:12:58 +0000

    input answer

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -5,5 +5,6 @@ char *_memset(char *s, char b, unsigned int n);
 void simple_print_buffer(char *buffer, unsigned int size);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
 
 #endif
