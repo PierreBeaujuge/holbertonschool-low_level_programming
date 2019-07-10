918683eeb9db38f1b522070bf8436b97ca1194ea
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-07-10 03:37:46 +0000

    input answer

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_chessboard - function that prints the chessboard
+ *
+ * @a: chessboard value
+ *
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+			if (j == 7)
+				_putchar('\n');
+		}
+	}
+}
