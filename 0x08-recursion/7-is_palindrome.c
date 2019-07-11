aaaa9e152f8d3410a1475313af1d724254dea596
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-07-11 05:31:06 +0000

    removed indentation

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -10,12 +10,12 @@
 
 int _strlen_recursion(char *s)
 {
-        if (*s == '\0')
-        {
-                return (0);
-        }
-        s++;
-        return (1 + _strlen_recursion(s));
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	s++;
+	return (1 + _strlen_recursion(s));
 }
 
 /**
