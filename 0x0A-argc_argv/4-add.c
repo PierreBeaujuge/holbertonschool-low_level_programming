0776bf9b8cfb49c863daf161fca00b7da7322189
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-07-14 22:02:09 +0000

    input answer again

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
 				if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
 				{
 					printf("Error\n");
-                                        return (1);
+					return (1);
 				}
 			}
 			sum += atoi(argv[i]);
