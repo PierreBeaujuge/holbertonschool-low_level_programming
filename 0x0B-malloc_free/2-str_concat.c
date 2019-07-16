8a9504c28767a346471e2fb2156fa12e10a95f3b
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-07-16 21:03:48 +0000

    added if NULL is passed, treat it as an empty string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,6 +22,11 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL && s2 == NULL)
 		return (NULL);
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	i = 0;
 	while (*s1++)
 		i++;
