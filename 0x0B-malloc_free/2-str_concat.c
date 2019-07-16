94995cb9e90138fdad74e1c3ff568835817776ff
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-07-16 21:13:07 +0000

    fixed indentation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,10 +19,6 @@ char *str_concat(char *s1, char *s2)
 	char *tmp1 = s1;
 	char *tmp2 = s2;
 
-/**
- *	if (s1 == NULL && s2 == NULL)
- *		return (NULL);
- */
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
