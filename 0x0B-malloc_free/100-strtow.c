366c49bc31253999da1081050d5f3394e070b725
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-07-18 05:00:25 +0000

    input answer

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,61 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * strtow - function that splits a string into words
+ *
+ * @str: pointer to the string for processing
+ *
+ * Return: pointer to an array of strings, address of the newly allocated memory
+ */
+
+char **strtow(char *str)
+{
+	int i, j, k, l, m;
+	int count = 0;
+	int len = 0;
+	char **words;
+
+	if (str == NULL || str == '\0')
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
+			count++;
+	}
+	if (count == 0)
+		return (NULL);
+
+	words = malloc((count + 1) * sizeof(char *));
+	if (words == NULL)
+	{
+		free(words);
+		return (NULL);
+	}
+
+	k = 0;
+	for (i = 0; str[i] != '\0' &&  k < count; i++)
+	{
+		for (; str[i] != ' '; k++)
+		{
+			len = 0;
+			for (j = i; str[j] != ' ' && str[j] != '\0'; j++)
+				len++;
+			words[k] = malloc((len + 1) * sizeof(char));
+			if (words[k] == NULL)
+			{
+				for (m = 0; m < k; m++)
+					free(words[k]);
+				free(words);
+				return (NULL);
+			}
+			for (l = 0; l < len; l++, i++)
+				words[k][l] = str[i];
+			words[k][l] = '\0';
+		}
+	}
+	words[k] = NULL;
+	return (words);
+}
