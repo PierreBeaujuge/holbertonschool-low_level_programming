e953ffdeb2c042c54e2349006a5ba3bda7b4ee33
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-07-19 02:22:47 +0000

    changed ptr NULL condition

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -4,6 +4,7 @@
 
 /**
  * _realloc - function that reallocates a memory block using malloc and free
+ * @ptr: pointer to the old array
  * @old_size: size of the memory space to allocate in bytes
  * @new_size: size of type
  * Return: void pointer
@@ -14,15 +15,24 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	void *newptr;
 
 	if (ptr == NULL)
-		return (NULL);
+	{
+		newptr = malloc(new_size);
+		if (newptr == NULL)
+		{
+			free(ptr);
+			return (NULL);
+		}
+		free(ptr);
+		return (newptr);
+	}
 	if (old_size == 0)
 		return (NULL);
 	if (new_size == old_size)
 		return (ptr);
-	if (new_size == 0)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
-		return(NULL);
+		return (NULL);
 	}
 	if (new_size > old_size)
 	{
@@ -53,15 +63,15 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-        unsigned int i;
-        char *tmp = dest;
+	unsigned int i;
+	char *tmp = dest;
 
-        for (i = 0; i < n; i++)
-        {
-                *dest = *src;
-                dest++;
-                src++;
-        }
-        dest = tmp;
-        return (dest);
+	for (i = 0; i < n; i++)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	dest = tmp;
+	return (dest);
 }
