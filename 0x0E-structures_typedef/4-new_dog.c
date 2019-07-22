130e9a97e43a409805ce766a5aa11ad859a39482
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-07-22 21:26:26 +0000

    made copies of name and owner

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,6 +14,7 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ptr;
+	char *tmp1 = name, *tmp2 = owner;
 
 	ptr = malloc(sizeof(dog_t));
 	if (ptr == NULL)
@@ -21,5 +22,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	ptr->name = name;
 	ptr->age = age;
 	ptr->owner = owner;
+	name = tmp1;
+	owner = tmp2;
 	return (ptr);
 }
