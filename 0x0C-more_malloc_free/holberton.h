0b1cd452ee7066e1a4b22dc26bcc24fbfaf0546f
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-07-22 00:23:19 +0000

    input answer

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -1,6 +1,10 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+void _error(void);
+int _isnumber(char *str);
+int _strlen(char *s);
+char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
