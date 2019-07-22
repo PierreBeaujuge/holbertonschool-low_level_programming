1cf3069cdd47b0c73cecd3f85c4708b6bfbd66fc
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-07-22 21:33:09 +0000

    input struct dog in malloc

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,15 +14,12 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ptr;
-	char *tmp1 = name, *tmp2 = owner;
 
-	ptr = malloc(sizeof(dog_t));
+	ptr = malloc(sizeof(struct dog));
 	if (ptr == NULL)
 		return (NULL);
 	ptr->name = name;
 	ptr->age = age;
 	ptr->owner = owner;
-	name = tmp1;
-	owner = tmp2;
 	return (ptr);
 }
