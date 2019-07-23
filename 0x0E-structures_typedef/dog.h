aada0f6e41481989b101c0bf1a51f6564bc7dea9
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-07-23 03:19:46 +0000

    input answer

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -25,5 +25,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 char *_strdup(char *str);
+void free_dog(dog_t *d);
 
 #endif
