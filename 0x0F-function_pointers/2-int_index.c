913b89efeba04ed6def2f2e7879e608a871b3f22
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-07-24 23:19:21 +0000

    added if condition

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -18,11 +18,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0)
 		return (-1);
 
-	for (i = 0; i < size; i++)
+	if ((*cmp) != NULL && array != NULL)
 	{
-		(*cmp)(array[i]);
-		if ((*cmp)(array[i]) != 0)
-			return (i);
+		for (i = 0; i < size; i++)
+		{
+			(*cmp)(array[i]);
+			if ((*cmp)(array[i]) != 0)
+				return (i);
+		}
 	}
 	return (-1);
 }
