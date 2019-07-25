d3f6eece522db4d32eb1ea5989288e975a02719f
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-07-25 00:04:28 +0000

    fixed *argv[3] in if statement

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,17 +18,17 @@ int main(int argc, char *argv[])
 	int num2;
 	int calc;
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-
 	if (argc != 4)
 		printf("Error\n"), exit(98);
 	if (*argv[2] != '+' && *argv[2] != '-' && *argv[2] != '*'
 	    && *argv[2] != '/' && *argv[2] != '%')
 		printf("Error\n"), exit(99);
-	if ((*argv[2] == '/' && argv[3] == NULL) ||
-	    (*argv[2] == '%' && argv[3] == NULL))
+	if ((*argv[2] == '/' && *argv[3] == 0) ||
+	    (*argv[2] == '%' && *argv[3] == 0))
 		printf("Error\n"), exit(100);
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
 	calc = (get_op_func(argv[2]))(num1, num2);
 	printf("%d\n", calc);
