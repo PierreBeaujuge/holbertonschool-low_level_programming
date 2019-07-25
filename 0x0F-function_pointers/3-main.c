799ac22d2ffe3337276ec35e0e7ffd2b14bc3146
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-07-25 00:54:32 +0000

    moved exit 100 to function file

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -19,17 +19,14 @@ int main(int argc, char *argv[])
 	int calc;
 	int (*ptr)(int, int);
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-
 	if (argc != 4)
 		printf("Error\n"), exit(98);
-	if ((*argv[2] == '/' && num2 == 0) || (*argv[2] == '%' && num2 == 0))
-		printf("Error\n"), exit(100);
 
 	ptr = get_op_func(argv[2]);
 	if (ptr != NULL)
 	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[3]);
 		calc = (*ptr)(num1, num2);
 		printf("%d\n", calc);
 		return (0);
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -52,7 +52,9 @@ int op_mul(int a, int b)
 
 int op_div(int a, int b)
 {
-	return (a / b);
+	if (b != 0)
+		return (a / b);
+	printf("Error\n"), exit(100);
 }
 
 /**
@@ -65,5 +67,7 @@ int op_div(int a, int b)
 
 int op_mod(int a, int b)
 {
-	return (a % b);
+	if (b != 0)
+		return (a % b);
+	printf("Error\n"), exit(100);
 }
