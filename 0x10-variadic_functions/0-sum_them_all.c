46f25206e450d443ffbaf03268e07870fc2bda16
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-07-27 21:43:47 +0000

    fixed synthax

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,7 +3,8 @@
 
 /**
  * sum_them_all - function that returns the sum of all its parameters
- * @n: const unsigned int passed from main, count argument indicating the number
+ * @n: const unsigned int passed from main,
+ * count argument indicating the number
  * of variadic arguments
  *
  * Return: sum of all the parameters or 0
@@ -16,10 +17,10 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 
 	if (n == 0)
-		return (0);
+		return(0);
 	va_start (args, n);
 	for (i = 0; i < n; i++)
 		sum += va_arg(args, int);
-	va_end(args);
+	va_end (args);
 	return(sum);
 }
