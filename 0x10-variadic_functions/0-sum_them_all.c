c664ebf9abe9b80db6906d4405eff5ef9503a322
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-07-27 21:45:49 +0000

    fixed synthax

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -17,10 +17,10 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 
 	if (n == 0)
-		return(0);
-	va_start (args, n);
+		return (0);
+	va_start(args, n);
 	for (i = 0; i < n; i++)
 		sum += va_arg(args, int);
-	va_end (args);
-	return(sum);
+	va_end(args);
+	return (sum);
 }
