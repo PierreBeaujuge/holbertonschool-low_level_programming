ad4d215c2e2ca624dbf606f5576aea59e47b4a58
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-07-27 21:53:30 +0000

    removed n=0 condition

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -5,7 +5,8 @@
 /**
  * print_strings - function that prints strings, followed by a new line
  * @separator: pointer to the separator
- * @n: const unsigned int passed from main, count argument indicating the number
+ * @n: const unsigned int passed from main,
+ * count argument indicating the number
  * of variadic arguments
  *
  * Return: void
@@ -17,22 +18,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	char *tmp;
 
-	if (n == 0)
-		return;
-	va_start (args, n);
+	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
 		tmp = va_arg(args, char *);
-/*
-		if (tmp == NULL)
-			printf("(nil)");
-*/
-
 		printf("%s", tmp != NULL ? tmp : "(nil)");
-/*
-		else
-			printf("%s", tmp);
-*/
 		if (i != n - 1)
 			if (separator)
 				printf("%s", separator);
