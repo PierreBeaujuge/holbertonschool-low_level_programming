f3cf22f430ef523afc180e7e89c1ba5b030a0127
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-07-27 21:50:56 +0000

    removed n=0 condition

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -5,7 +5,8 @@
 /**
  * print_numbers - function that prints numbers, followed by a new line
  * @separator: pointer to the separator
- * @n: const unsigned int passed from main, count argument indicating the number
+ * @n: const unsigned int passed from main,
+ * count argument indicating the number
  * of variadic arguments
  *
  * Return: void
@@ -16,14 +17,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 	unsigned int i;
 
-	if (n == 0)
-		return;
-	va_start (args, n);
+	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
 		printf("%i", va_arg(args, int));
 		if (i != n - 1)
-			if(separator)
+			if (separator)
 				printf("%s", separator);
 	}
 	printf("\n");
