11195002f7ad2783d32a97460873beb7eb4b4508
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-08-06 17:28:21 +0000

    add if head == NULL then return (NULL);

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,6 +20,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new;
 	listint_t *temp;
 
+	if (head == NULL)
+		return (NULL);
 	if (*head == NULL)
 		return (NULL);
 
