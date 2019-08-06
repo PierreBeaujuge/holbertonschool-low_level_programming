f2ce108665cf22c961131adf590c4f9fd873585c
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-08-06 16:43:48 +0000

    readd *head = NULL end statement, add if *head == NULL then return;

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -14,8 +14,8 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if (head == NULL)
-		return (NULL);
+	if (*head == NULL)
+		return;
 
 	while (*head != NULL)
 	{
@@ -23,4 +23,5 @@ void free_listint2(listint_t **head)
 		*head = (*head)->next;
 		free(temp);
 	}
+	*head = NULL;
 }
