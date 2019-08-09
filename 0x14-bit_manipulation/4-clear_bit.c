19d3a8686951b37673e5cf565ca43ce77097c942
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-08-09 03:17:44 +0000

    added parentheses before ~

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -13,6 +13,6 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
-	*n = *n & ~1 << index;
+	*n = *n & ~(1 << index);
 	return (1);
 }
