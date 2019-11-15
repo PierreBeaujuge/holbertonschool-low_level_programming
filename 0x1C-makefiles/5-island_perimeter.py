74ac4c4719c854e53406d44306b3804650720e9f
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-11-15 22:44:27 +0000

    input answer

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Island Perimeter
+"""
+
+def island_perimeter(grid):
+    """function that returns the perimeter of the island described in grid"""
+    count = 0
+    for i in range(0, len(grid)):
+        for j in range(0, len(grid[i])):
+            if grid[i][j] == 1:
+                count += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    count -= 1
+                if i < len(grid) - 1 and grid[i + 1][j] == 1:
+                    count -= 1
+                if j > 0 and grid[i][j - 1] == 1:
+                    count -= 1
+                if j < len(grid[i]) - 1 and grid[i][j + 1] == 1:
+                    count -= 1
+    return count
