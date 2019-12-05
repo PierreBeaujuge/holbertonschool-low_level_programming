025c4874a39012fbaad017d735430f98046510d4
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-12-05 22:21:11 +0000

    input answer

---
+++ 0x1D-binary_trees/110-binary_tree_is_bst.c
@@ -0,0 +1,46 @@
+#include "binary_trees.h"
+
+int is_bst(const binary_tree_t *tree, int data);
+
+/**
+ * binary_tree_is_bst - function that checks if a binary tree
+ * is a valid Binary Search Tree
+ * @tree: a pointer to the root node of the tree to check
+ * Return: 1 if tree is a valid BST, and 0 otherwise
+ * If tree is NULL, return 0
+ */
+
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (is_bst(tree, tree->n));
+}
+
+/**
+ * is_bst - function that checks if a binary tree
+ * is a valid Binary Search Tree
+ * @tree: a pointer to the root node of the tree to check
+ * @data: n value of root passed from main
+ * Return: 1 if tree is a valid BST, and 0 otherwise
+ * If tree is NULL, return 0
+ */
+
+int is_bst(const binary_tree_t *tree, int data)
+{
+	if (!tree)
+		return (0);
+	if (tree->left->n < tree->n && tree->right->n > tree->n)
+		if ((!tree->left->left ||
+		     (tree->left->left && tree->left->left->n < data))
+		    && (!tree->left->right ||
+			(tree->left->right && tree->left->right->n < data))
+		    && (!tree->right->left ||
+			(tree->right->left && tree->right->left->n > data))
+		    && (!tree->right->right ||
+			(tree->right->right && tree->right->right->n > data)))
+		return (1);
+	is_bst(tree->left, data);
+	is_bst(tree->right, data);
+	return (0);
+}
