b12b5fc5bb6849fcde153e9e003748c2e9fd1034
PierreBeaujuge <pierre.beaujuge@gmail.com>
2019-12-07 07:21:09 +0000

    fixed the binary_tree_is_full function

--- 0x1D-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -38,7 +38,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	if (!tree->left || !tree->right)
+	if (!tree->left && !tree->right)
 		return (0);
 	left_bool = 1 + binary_tree_is_full(tree->left);
 	right_bool = 1 + binary_tree_is_full(tree->right);
