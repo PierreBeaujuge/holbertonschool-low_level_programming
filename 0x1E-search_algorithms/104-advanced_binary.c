350502af5c064cbcae4cc1b7570fd7e8ca68c6d9
PierreBeaujuge <pierre.beaujuge@gmail.com>
2020-01-17 07:51:38 +0000

    fixed betty req

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -3,7 +3,7 @@
 int binary_search_recurse(int *array, int start, int end, int value);
 
 /**
- * advanced_search - function that searches for a value in a sorted array of
+ * advanced_binary - function that searches for a value in a sorted array of
  * integers using the Binary search algorithm, recursively
  * @array: pointer to the first element of the array to search in
  * @size: number of elements in array
@@ -21,22 +21,31 @@ int advanced_binary(int *array, size_t size, int value)
 	return (i);
 }
 
+/**
+ * binary_search_recurse - recursion function
+ * @array: pointer to the first element of the array to search in
+ * @start: start index
+ * @end: end index
+ * @value: value is the value to search for
+ * Return: first index where value is located
+ */
+
 int binary_search_recurse(int *array, int start, int end, int value)
 {
 	int i, mid;
 
 	if (end < start)
 		return (-1);
-        printf("Searching in array: ");
-        for (i = start; i <= end; i++)
-        {
-                printf("%i", array[i]);
-                if (i != end)
-                        printf(", ");
-                else
-                        printf("\n");
-        }
-        mid = (start + end) / 2;
+	printf("Searching in array: ");
+	for (i = start; i <= end; i++)
+	{
+		printf("%i", array[i]);
+		if (i != end)
+			printf(", ");
+		else
+			printf("\n");
+	}
+	mid = (start + end) / 2;
 	if (array[mid] == value)
 	{
 		if (array[mid - 1] == value)
